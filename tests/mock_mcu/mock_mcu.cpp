f824804c7b984838c9d2e2730f71f39c74140d17
Daryl Stark <daryl@dstark.nl>
2025-10-20 15:17:01 +0000

    feat: Implement NTP client tests.

--- tests/mock_mcu/mock_mcu.cpp
+++ tests/mock_mcu/mock_mcu.cpp
@@ -1,16 +1,27 @@
 #include "mock_mcu.hpp"
 
 MockMCU::MockMCU(const MockMCUConfiguration config)
-    : _gpio_bank(config.gpio_count)
+    : _gpio_bank(config.gpio_count), _time_synced(false)
 {
 }
 
 void MockMCU::clear()
 {
     _gpio_bank.clear();
+    _time_synced = false;
 }
 
 GPIOBank &MockMCU::gpio_bank()
 {
     return _gpio_bank;
+}
+
+void MockMCU::set_time_synced(bool synced)
+{
+    _time_synced = synced;
+}
+
+bool MockMCU::get_time_sync_status() const
+{
+    return _time_synced;
 }
--- tests/mock_mcu/mock_mcu.hpp
+++ tests/mock_mcu/mock_mcu.hpp
@@ -11,10 +11,14 @@ class MockMCU
 {
 private:
     GPIOBank _gpio_bank;
+    bool _time_synced;
 
 public:
     MockMCU(const MockMCUConfiguration config);
     void clear();
 
     GPIOBank &gpio_bank();
+
+    void set_time_synced(bool synced);
+    bool get_time_sync_status() const;
 };
